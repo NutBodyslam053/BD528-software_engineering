c823e6ad82fc945cb162fe0afcad75e8494d3b18
NutBodyslam053 <seklosoann@gmail.com>
2023-10-02 20:21:47 +0700

    stage-01 ingestion

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BD528-software_engineering
 
-## Work Flow
+## Workflow
 
 ### 0. Create Github repository & Clone to local repository
 ```bash
@@ -20,8 +20,18 @@ conda activate venv/
 pip install -r requirements.txt
 ```
 
-### 3. Initialize constructor for logging (src/mlProject/__init__.py)
-
+### 3. Initialize logger using constructor
 ```bash
-
+src/mlProject/__init__.py
 ```
+
+### 4. Create source code
+    4.1 Update config.yaml
+    4.2 Update schema.yaml
+    4.3 Update params.yaml
+    4.4 Update the entity
+    4.5 Update the config
+    4.6 Update the components
+    4.7 Update the pipeline
+    4.8 Update the main.py
+    4.9 Update the app.py
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+# from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+# from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+# from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "DATA INGESTION"
+try:
+    logger.info(f"=============== Stage :: {STAGE_NAME} :: Started! ===============")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"=============== Stage :: {STAGE_NAME} :: Completed! ===============")
+    print("\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
