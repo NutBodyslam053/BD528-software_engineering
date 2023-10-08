1c4126bf9f454a3cca43c60a308db77598ec6818
NutBodyslam053 <seklosoann@gmail.com>
2023-10-08 15:43:25 +0700

    add data validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -10,6 +10,18 @@ STAGE_NAME = "DATA INGESTION"
 try:
     logger.info(f"=============== Stage :: {STAGE_NAME} :: Started! ===============")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"=============== Stage :: {STAGE_NAME} :: Completed! ===============")
+    print("\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "DATA VALIDATION"
+try:
+    logger.info(f"=============== Stage :: {STAGE_NAME} :: Started! ===============")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f"=============== Stage :: {STAGE_NAME} :: Completed! ===============")
     print("\n")
---
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -0,0 +1,16 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_validation import DataValidation
+from mlProject import logger
+
+
+STAGE_NAME = "Data Validation"
+
+class DataValidationTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+        
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
